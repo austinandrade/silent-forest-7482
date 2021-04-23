1a413e69aba43f00662f8a9d6b695363e06145df
austinandrade <austinmandrade@gmail.com>
2021-04-23 00:06:57 -0400

    Create most_popular ingredients method and test

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,5 +1,14 @@
 class Chef < ApplicationRecord
   validates_presence_of :name
   has_many :dishes
+  has_many :dishes_ingredients, through: :dishes
+  has_many :ingredients, through: :dishes_ingredients
 
+  def most_popular_ingredients
+    ingredients
+    .select('ingredients.*, count(ingredients) as ingred_usage')
+    .group('ingredients.id')
+    .order(ingred_usage: :desc)
+    .limit(3)
+  end
 end
--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -7,4 +7,38 @@ RSpec.describe Chef, type: :model do
   describe "relationships" do
     it {should have_many :dishes}
   end
+
+  before(:each) do
+    @chef = Chef.create!(name: 'Gordon Ramsey')
+
+    @ingredient = Ingredient.create!(name: 'Siracha', calories: 50)
+    @ingredient_2 = Ingredient.create!(name: 'Beans', calories: 100)
+    @ingredient_3 = Ingredient.create!(name: 'Rice', calories: 25)
+    @ingredient_4 = Ingredient.create!(name: 'Corn', calories: 10)
+
+    @dish_1 = @chef.dishes.create!(name: 'pasta', description: 'yummy pasta')
+    @dish_2 = @chef.dishes.create!(name: 'chicken', description: 'great chicken')
+    @dish_3 = @chef.dishes.create!(name: 'apples', description: 'crisp apples')
+    @dish_4 = @chef.dishes.create!(name: 'candy', description: 'unhealthy')
+    @dish_5 = @chef.dishes.create!(name: 'yummy', description: 'this is really great')
+    @dish_6 = @chef.dishes.create!(name: 'tasty', description: 'wow its awesome')
+
+    DishesIngredient.create!(dish: @dish_1, ingredient: @ingredient)
+    DishesIngredient.create!(dish: @dish_2, ingredient: @ingredient)
+    DishesIngredient.create!(dish: @dish_3, ingredient: @ingredient_2)
+    DishesIngredient.create!(dish: @dish_5, ingredient: @ingredient_2)
+    DishesIngredient.create!(dish: @dish_6, ingredient: @ingredient_2)
+    DishesIngredient.create!(dish: @dish_1, ingredient: @ingredient_3)
+    DishesIngredient.create!(dish: @dish_2, ingredient: @ingredient_3)
+    DishesIngredient.create!(dish: @dish_3, ingredient: @ingredient_3)
+    DishesIngredient.create!(dish: @dish_4, ingredient: @ingredient_3)
+  end
+
+  describe 'instance methods' do
+    describe '#most_popular_ingredients' do
+      it "displays the chef's three ingredients in descending order" do
+        expect(@chef.most_popular_ingredients).to eq([@ingredient_3, @ingredient_2, @ingredient])
+      end
+    end
+  end
 end
